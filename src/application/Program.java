c1672a74a1949fb7b28d6093fc453ddc5af96a67
Kawan Diego <kawandiego42@gmail.com>
2023-03-16 15:44:01 -0300

    Criando BoardException para tratamento de exceção e utilizando programação defensiva

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,9 +6,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		UI ui = new UI();
 		ChessMatch chessMatch = new ChessMatch();
-		ui.printBoard(chessMatch.getPieces());
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
